91746d3908b5f03e110d0a0049e745e287d9674d
nastya <nastya@yourockdude.com>
2017-11-03 20:58:18 +0300

    commit

---
+++ app/src/main/java/com/example/bloold/buildp/map/BigClusteringDemoActivity.java
@@ -0,0 +1,320 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bloold.buildp.map;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.bloold.buildp.Modules.DirectionFinderListener;
+import com.example.bloold.buildp.Modules.DirectionFinder;
+import com.example.bloold.buildp.Modules.Route;
+import com.example.bloold.buildp.R;
+import com.example.bloold.buildp.model.MyItem;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.gson.Gson;
+import com.google.maps.android.clustering.ClusterManager;
+//import com.google.maps.android.utils.demo.model.MyItem;
+
+public class BigClusteringDemoActivity extends BaseDemoActivity implements DirectionFinderListener {
+    private ClusterManager<MyItem> mClusterManager;
+    private Button btnFindPath;
+    private EditText etOrigin;
+    private EditText etDestination;
+    private List<Marker> originMarkers = new ArrayList<>();
+    private List<Marker> destinationMarkers = new ArrayList<>();
+    private List<Polyline> polylinePaths = new ArrayList<>();
+    private ProgressDialog progressDialog;
+    double lng1 = 16.85176189428896;
+    double lng2 = 148.6877064739115;
+    double lat1 = -30.4486834932737;
+    double lat2 = 85.0511287798066;
+
+    @Override
+    protected void startDemo() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        ChangeMap();
+        mClusterManager = new ClusterManager<MyItem>(this, getMap());
+        getMap().setOnCameraIdleListener(mClusterManager);
+        btnFindPath = (Button) findViewById(R.id.btnFindPath);
+        etOrigin = (EditText) findViewById(R.id.etOrigin);
+        etDestination = (EditText) findViewById(R.id.etDestination);
+
+        btnFindPath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
+    }
+
+    private void ChangeMap(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getMap().setMyLocationEnabled(true);
+            LocationManager locationManager = (LocationManager)
+                    getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+
+            Location location = locationManager.getLastKnownLocation(locationManager
+                    .getBestProvider(criteria, false));
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 12));
+            LatLngBounds bounds = getMap().getProjection().getVisibleRegion().latLngBounds;
+            lat1 = bounds.southwest.latitude;
+            lat2 = bounds.northeast.latitude;
+
+            lng1 = bounds.southwest.longitude;
+            lng2 = bounds.northeast.longitude;
+            if (lat1<lat2){
+                double tmp  = lat1;
+                lat1 = lat2;
+                lat1 =tmp;
+            }
+
+            if (lng1<lng2){
+                double tmp  = lng1;
+                lng1 = lng2;
+                lng1 =tmp;
+            }
+        } else {
+            // Toast.makeText(this, "Please enter origin address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        new JSONTask().execute("http://ruinnet.idefa.ru/api_app/object/index/?select[]=ID&select[]=NAME&select[]=ADDRESS&select[]=MAP_TYPE&select[]=LAT&select[]=LNG&filter[INCLUDE_SUBSECTIONS]=Y&filter[%3C%3DLAT_FROM]="+lat2+"&filter[%3E%3DLAT_TO]="+lat1+"&filter[%3C%3DLNG_FROM]="+lng2+"&filter[%3E%3DLNG_TO]="+lng1+"&limit=3500&page=1");
+    }
+
+    private void sendRequest() {
+        String origin = etOrigin.getText().toString();
+        String destination = etDestination.getText().toString();
+        if (origin.isEmpty()) {
+            Toast.makeText(this, "Please enter origin address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (destination.isEmpty()) {
+            Toast.makeText(this, "Please enter destination address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            new DirectionFinder(this, origin, destination).execute();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void onDirectionFinderStart() {
+        progressDialog = ProgressDialog.show(this, "Please wait.",
+                "Finding direction..!", true);
+
+        if (originMarkers != null) {
+            for (Marker marker : originMarkers) {
+                marker.remove();
+            }
+        }
+
+        if (destinationMarkers != null) {
+            for (Marker marker : destinationMarkers) {
+                marker.remove();
+            }
+        }
+
+        if (polylinePaths != null) {
+            for (Polyline polyline:polylinePaths ) {
+                polyline.remove();
+            }
+        }
+    }
+
+    @Override
+    public void onDirectionFinderSuccess(List<Route> routes) {
+        progressDialog.dismiss();
+        polylinePaths = new ArrayList<>();
+        originMarkers = new ArrayList<>();
+        destinationMarkers = new ArrayList<>();
+
+        for (Route route : routes) {
+            getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(route.startLocation, 16));
+            ((TextView) findViewById(R.id.tvDuration)).setText(route.duration.text);
+            ((TextView) findViewById(R.id.tvDistance)).setText(route.distance.text);
+
+            originMarkers.add(getMap().addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.start_blue))
+                    .title(route.startAddress)
+                    .position(route.startLocation)));
+            destinationMarkers.add( getMap().addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.end_green))
+                    .title(route.endAddress)
+                    .position(route.endLocation)));
+
+            PolylineOptions polylineOptions = new PolylineOptions().
+                    geodesic(true).
+                    color(Color.BLUE).
+                    width(10);
+
+            for (int i = 0; i < route.points.size(); i++)
+                polylineOptions.add(route.points.get(i));
+
+            polylinePaths.add( getMap().addPolyline(polylineOptions));
+        }
+    }
+    public class JSONTask extends AsyncTask<String,String, List<MyItem> > {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //dialog.show();
+        }
+
+        @Override
+        protected List<MyItem> doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Device-Id","0000");
+                String header = "Basic " + new String(android.util.Base64.encode("defa:defa".getBytes(), android.util.Base64.NO_WRAP));
+                connection.addRequestProperty("Authorization", header);
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line ="";
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line);
+                }
+
+                String finalJson = buffer.toString();
+                JSONArray ITEMS = null;
+                JSONObject DATA = null;
+                JSONObject response = null;
+                response = new JSONObject(finalJson);
+                try {
+                    DATA = new JSONObject(response.getString("DATA"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    ITEMS = new JSONArray(DATA.getString("ITEMS"));
+                    //parentArray = DATA.getJSONArray("ITEMS");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                List<MyItem> items = new MyItemReader().read(ITEMS);
+                mClusterManager.addItems(items);
+                /*for (int i = 0; i < 10; i++) {
+                    double offset = i / 60d;
+                    for (MyItem item : items) {
+                        LatLng position = item.getPosition();
+                        double lat = position.latitude + offset;
+                        double lng = position.longitude + offset;
+                        MyItem offsetItem = new MyItem(lat, lng);
+                        mClusterManager.addItem(offsetItem);
+                    }
+                }*/
+
+                } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+    }
+
+   /* private void readItems() throws JSONException {
+        InputStream inputStream = getResources().openRawResource(R.raw.radar_search);
+        List<MyItem> items = new MyItemReader().read(inputStream);
+        for (int i = 0; i < 10; i++) {
+            double offset = i / 60d;
+            for (MyItem item : items) {
+                LatLng position = item.getPosition();
+                double lat = position.latitude + offset;
+                double lng = position.longitude + offset;
+                MyItem offsetItem = new MyItem(lat, lng);
+                mClusterManager.addItem(offsetItem);
+            }
+        }
+    }*/
+}
---
+++ app/src/main/java/com/example/bloold/buildp/map/MyItemReader.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bloold.buildp.map;
+
+import com.example.bloold.buildp.model.MyItem;
+//import com.google.maps.android.utils.demo.model.MyItem;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MyItemReader {
+
+    /*
+     * This matches only once in whole input,
+     * so Scanner.next returns whole InputStream as a String.
+     * http://stackoverflow.com/a/5445161/2183804
+     */
+    private static final String REGEX_INPUT_BOUNDARY_BEGINNING = "\\A";
+
+    public List<MyItem> read(JSONArray finalJson) throws JSONException {
+        List<MyItem> items = new ArrayList<MyItem>();
+        for (int i = 0; i < finalJson.length(); i++) {
+            String title = null;
+            String snippet = null;
+            JSONObject object = finalJson.getJSONObject(i);
+            double lat = object.getDouble("LAT");
+            double lng = object.getDouble("LNG");
+            if (!object.isNull("title")) {
+                title = object.getString("ADDRESS");
+            }
+            if (!object.isNull("snippet")) {
+                snippet = object.getString("NAME");
+            }
+            items.add(new MyItem(lat, lng, title, snippet));
+        }
+        return items;
+    }
+
+}
